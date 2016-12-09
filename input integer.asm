33c3ac7817c41b44e6c5dd1d825c867bd7f60ada
xieleigang <xieleigang@gmail.com>
2016-12-08 23:01:37 -0600

    Add files via upload

--- input integer.asm
+++ input integer.asm
@@ -1,17 +1,18 @@
     .data
-op1: .word  10
-op2: .word  20
-space: .asciiz " " #space to insert between numbers
+    op1: .word  -2147483648
+    op2: .word  999
+    space: .asciiz "\n" 
 
     .text
     la $t0, op1  # load address
     la $t1, op2
+	
     #load word
-    lw  $t2, 0($t0)
+    lw $t2, 0($t0)
     lw $t3, 0($t1)
     
     #print op1 in binary
-    move $a0 , $t2
+    move $a0, $t2
     li $v0, 35
     syscall
     
@@ -20,9 +21,20 @@ space: .asciiz " " #space to insert between numbers
     li $v0, 4
     syscall
     
-    #print op2
+    #print op2 in binary
     move $a0, $t3
     li $v0 35
     syscall
-    
+	
+    #print space
+    la $a0, space
+    li $v0, 4
+    syscall
+
+    #print res in binary	
+    div $t2, $t3
+    mflo $a0
+    li $v0, 35
+    syscall
+
     
